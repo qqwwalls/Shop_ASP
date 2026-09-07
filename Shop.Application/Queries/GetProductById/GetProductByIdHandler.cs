7609c8652b922daa2d60652cece3dff5e956191a
Olena <olena@example.com>
2026-09-07 19:48:47 +0300

    add logging to CQRS handler

--- Shop.Application/Queries/GetProductById/GetProductByIdHandler.cs
+++ Shop.Application/Queries/GetProductById/GetProductByIdHandler.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using Shop.Application.DTOs;
 using Shop.Application.Interfaces.Repository;
 using System.Threading;
@@ -11,15 +12,19 @@ namespace Shop.Application.Queries.GetProductById
     {
         private readonly IProductRepository _repository;
         private readonly IMapper _mapper;
+        private readonly ILogger<GetProductByIdHandler> _logger;
 
-        public GetProductByIdHandler(IProductRepository repository, IMapper mapper)
+        public GetProductByIdHandler(IProductRepository repository, IMapper mapper, ILogger<GetProductByIdHandler> logger)
         {
             _repository = repository;
             _mapper = mapper;
+            _logger = logger;
         }
 
         public async Task<ProductDto?> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("🚀 [CQRS] GetProductByIdHandler was triggered for Product ID: {Id}", request.Id);
+
             // Отримуємо сутність з БД через репозиторій (як на скріншоті)
             var entity = _repository.GetProductById(request.Id);
             
